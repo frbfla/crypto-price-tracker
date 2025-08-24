cf4ade3783cc85f97227cec5118183c4bcabb38b
fillipe.bernardes@tre-rj.jus.br <fillipe.bernardes@tre-rj.jus.br>
2025-08-24 18:30:28 -0300

    Ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
